48d6359fd8cb6bd5e9f01debd965c573766b56e0
Hernan Paez <lhernan.paez@gmail.com>
2019-02-23 19:01:50 -0300

    Initial Commit

---
+++ LocationHistoryAnimated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationHistoryAnimated
+//
+//  Created by Hernan Paez on 23/02/2019.
+//  Copyright © 2019 InfinixSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
